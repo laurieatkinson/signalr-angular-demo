3e15e8d62e94e2c609fad6f3da24194e9591b13e
Laurie Atkinson <laurieat@microsoft.com>
2022-11-30 21:40:30 -0700

    add reconnection logic

--- SignalRAngularDemo.Client/src/app/services/signalr.service.ts
+++ SignalRAngularDemo.Client/src/app/services/signalr.service.ts
@@ -7,31 +7,43 @@ import { IAccountModel } from '../models/account.model';
   providedIn: 'root'
 })
 export class SignalrService {
-  // private activeAccountsSubject = new Subject<IAccountModel[]>();  
-  // private connectionEstablishedSubject = new Subject<boolean>();
-  // private activeAccounts: IAccountModel[];
+  private activeAccountsSubject = new Subject<IAccountModel[]>();  
+  private connectionEstablishedSubject = new Subject<boolean>();
+  private connectionReconnectingSubject = new Subject<string>();
 
-  // activeAccounts$ = this.activeAccountsSubject.asObservable();
-  // connectionEstablished$ = this.connectionEstablishedSubject.asObservable();
+  activeAccounts$ = this.activeAccountsSubject.asObservable();
+  connectionEstablished$ = this.connectionEstablishedSubject.asObservable();
+  connectionReconnecting$ = this.connectionReconnectingSubject.asObservable();
 
-  activeAccounts: IAccountModel[];
+  // activeAccounts: IAccountModel[];
 
   private hubConnection: signalR.HubConnection
     startConnection = () => {
       this.hubConnection = new signalR.HubConnectionBuilder()
                               .withUrl('https://localhost:5001/account')
+                              .withAutomaticReconnect()
                               .build();
       this.hubConnection
         .start()
         .then(() => console.log('Connection started'))
-        .catch(err => console.log('Error while starting connection: ' + err))
+        .catch(err => console.log('Error while starting connection: ' + err));
+
+      this.hubConnection.onreconnecting(error => {
+        console.assert(this.hubConnection.state === signalR.HubConnectionState.Reconnecting);
+        this.connectionReconnectingSubject.next(error.message);
+      });
+
+      this.hubConnection.onreconnected(connectionId => {
+        console.assert(this.hubConnection.state === signalR.HubConnectionState.Connected);
+        this.connectionReconnectingSubject.next(null);
+      });
     }
     
     addTransferActiveAccountsData = () => {
       this.hubConnection
         .on('TransferActiveAccountsData', (data: IAccountModel[]) => {
-          this.activeAccounts = data;
-          // this.activeAccountsSubject.next([...data]);
+          // this.activeAccounts = data;
+          this.activeAccountsSubject.next([...data]);
           console.log(data);
         });
     }
@@ -45,8 +57,8 @@ export class SignalrService {
     addBroadcastAccountDataListener = () => {
       this.hubConnection
         .on('BroadcastActiveAccountData', (data: IAccountModel[]) => {
-          this.activeAccounts = data;
-          // this.activeAccountsSubject.next([...data]);
+          // this.activeAccounts = data;
+          this.activeAccountsSubject.next([...data]);
           console.log(data);
         })
     }
